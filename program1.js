1f292c3e0dbf519e0c46688fb57002a8f8d06b88
rohini-sharma-04 <168103373+rohini-sharma-04@users.noreply.github.com>
2024-04-26 06:51:29 +0000

    Auto commit on save

--- program1.js
+++ program1.js
@@ -4,7 +4,7 @@ function smallestMissingPositiveInteger(nums) {
     // Implement the function smallest_missing_positive_integer
     nums = nums.filter(nums=> num >0) ;
     for(let i=0 ; i<nums.length ; i++){
-      let index = Math.a
+      let index = Math.abs
     }
   
   }
