e40576436afbeb6d7395e7f005db7f671b269ce7
rohini-sharma-04 <168103373+rohini-sharma-04@users.noreply.github.com>
2024-04-26 07:07:04 +0000

    Auto commit on save

--- program2.js
+++ program2.js
@@ -5,7 +5,7 @@ function longestSubstring(s) {
     let longestSubstringLength = 0 ;
     for(let i=0 ; i<s.length; i++){
         let currentStringSet = new Set() ;
-        for(let x=i ; x<s.length() ; i++){
+        for(let x=i ; x<s.length() ; x++){
             if(currentStringSet.has(s[x])){
                 break ;
             }
