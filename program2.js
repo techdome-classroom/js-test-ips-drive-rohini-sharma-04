e6465bbb3450dc2a2fd60df903efa681d1107691
rohini-sharma-04 <168103373+rohini-sharma-04@users.noreply.github.com>
2024-04-26 06:59:03 +0000

    Auto commit on save

--- program2.js
+++ program2.js
@@ -3,7 +3,7 @@ function longestSubstring(s) {
 
     // Implementation of longestSubstring function
     let longestSubstringLength = 0 ;
-    for(int i=0 )
+    for(int i=0 ; i<s.length ;i++)
 }
 
 module.exports = { longestSubstring };
